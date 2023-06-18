fb7f410058c10dabf255eaddddd9f2ee900d36fe
DannyChen <dannyisadog10@gmail.com>
2023-06-18 23:39:04 +0800

    feat: set wording when matching

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ const main = async () => {
     });
   });
 
-  server.listen(80, () => {
-    console.log('listening on *:80');
+  server.listen(3000, () => {
+    console.log('listening on *:3000');
   });
 }
 
